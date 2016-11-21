1fc2c90f25b3f3ca59ccfc792983a42d67a6450a
pablogarin <pablo.garin@hotmail.com>
2016-11-21 03:48:11 +0000

    Password update added

--- html/admin/sections/config.php
+++ html/admin/sections/config.php
@@ -1,5 +1,27 @@
 <?php
-
+if( isset($_REQUEST['cambia-pass']) ){
+    if( !empty($_POST['actual']) && !empty($_POST['nueva']) && !empty($_POST['repeat']) ) {
+        $cur = $dbh->query("SELECT * FROM usuario WHERE id=? and password=?;", array($_SESSION['user'],$_POST['actual']));
+        if( empty($cur) ){
+            session_destroy();
+            header("Location: /");
+            exit();
+        }
+        if( $_POST['nueva']!==$_POST['repeat'] ){
+            $view->set("error","Las contrase&ntilde;as no coinciden entre si.");
+        } else {
+            $id = $cur[0]['id'];
+            $cur = $dbh->query("UPDATE usuario SET password=? WHERE id=?;", array($_POST['nueva'], $id));
+            $view->set("success","Contrase&ntilde;a actualizada exitosamente.");
+        }
+    } else {
+        $view->set("error","Debe ingresar todos los campos.");
+    }
+} else {
+    unset($_POST['actual']);
+    unset($_POST['nueva']);
+    unset($_POST['repeat']);
+}
 if( isset($_REQUEST['grabar']) ){
     $query = "UPDATE config SET valor=? WHERE id=?;";
     foreach( $_POST['configs'] as $id=>$valor ){
